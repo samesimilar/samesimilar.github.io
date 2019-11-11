5960522f09e17e3f12138815a21346ebacb13a53
Mike Spears <mike.spears@utoronto.ca>
2019-11-11 18:01:30 -0500

    added twin story format file

---
+++ format.js
@@ -0,0 +1,8 @@
+window.storyFormat({
+	"author": "Mike Spears",
+	"description": "A new story format for VR walkthroughs.",
+	"image": "icon.svg",
+	"name": "reach",
+	"proofing": false,
+	"source": "<!DOCTYPE html>\n<html>\n  <head>\n    <title>{{STORY_NAME}}</title>\n\n</head><body>{{STORY_DATA}}</body></html>"
+});
