8e996415b0c4bebfee50802ae0a15478134f1586
Mike Spears <mike.spears@utoronto.ca>
2019-11-11 21:52:48 -0500

    added script

--- format.js
+++ format.js
@@ -7,5 +7,5 @@ window.storyFormat({
 	"proofing": false,
 	"license": "MIT",
 	"url": "https://samesimilar.com",
-	"source": "<!DOCTYPE html>\n<html>\n  <head>\n    <title>{{STORY_NAME}}</title>\n\n</head><body>{{STORY_DATA}}</body></html>"
+	"source": "<!DOCTYPE html>\n<html>\n  <head>\n    <script src=\"https://aframe.io/releases/0.9.2/aframe.min.js\"></script>\n    <script src=\"reach.js\"></script>\n    <title>{{STORY_NAME}}</title>\n  </head>\n  <body>\n    {{STORY_DATA}}\n    <a-scene>\n      <a-assets> </a-assets>\n      <a-entity id=\"container\"> </a-entity>\n      <a-entity laser-controls=\"hand: right\"></a-entity>\n      <a-camera id=\"main-camera\" wasd-controls=\"enabled: false\">\n        <a-cursor objects=\".clickable\"></a-cursor>\n      </a-camera>\n    </a-scene>\n\n    <script>\n      loadLocalStory();\n    </script>\n  </body>\n</html>\n\n"
 });
